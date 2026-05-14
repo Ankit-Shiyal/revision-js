f4883bfcfa2d51bf4d1331da107a0b835084f682
ankit shiyal <ankitshiyal2005@gmail.com>
2026-05-14 10:32:20 +0530

    const added

---
+++ 02-js_introduction/3-const.js
@@ -0,0 +1,34 @@
+
+// const is a function Scope
+
+
+function message(){
+
+    const msg ="hello"
+
+    if(true){
+        console.log("in if condition", msg)
+    }
+
+
+}
+
+//  console.log("in if condition", msg)
+message()
+
+// declaration and initialization
+const name="ankit"
+console.log(name)
+
+// name ="rutik"
+
+// console.log(name)
+
+// const age;
+
+// age=20
+
+// console.log(age)
+
+
+
