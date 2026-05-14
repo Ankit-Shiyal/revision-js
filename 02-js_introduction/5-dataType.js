c4aac318d1dbe5440cc7dfb9e44f0526010268ee
ankit shiyal <ankitshiyal2005@gmail.com>
2026-05-14 10:39:44 +0530

    data type added

---
+++ 02-js_introduction/5-dataType.js
@@ -0,0 +1,31 @@
+
+//primitive data type
+
+
+//1.string
+
+let string = "Ankit";
+
+console.log(string);
+console.log(typeof string);
+
+//2.number
+
+let number = 11281;
+
+console.log(number);
+console.log(typeof number);
+
+//3.undefine
+
+let a;
+console.log(typeof a);
+
+
+//4.Boolean
+
+let A = true;
+
+console.log(A);
+console.log(typeof A);
+
