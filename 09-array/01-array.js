87461902d5e07b6b4bdb96f519f9ca3ed90013a7
ankit shiyal <ankitshiyal2005@gmail.com>
2026-06-03 10:09:34 +0530

    array  declaration added

---
+++ 09-array/01-array.js
@@ -0,0 +1,19 @@
+
+
+// using array syntax
+
+const fruits =["apple", "banana", "guava", "mango"]
+
+console.log("array of fruits", fruits)
+
+console.log("type",typeof(fruits))
+
+
+// using 
+
+const variable = new Array("potato", "tomato", "ginger", "chilli")
+
+console.log("array of variable", variable)
+
+console.log("type variable array",typeof(fruits))
+
