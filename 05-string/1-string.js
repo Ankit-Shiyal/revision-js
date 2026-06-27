76544e2b45ced170ce15fd5fb997ab2c78932938
ankit shiyal <ankitshiyal2005@gmail.com>
2026-06-27 20:26:23 +0530

    string added

---
+++ 05-string/1-string.js
@@ -0,0 +1,33 @@
+
+
+let all = "Hello My name is Ankit"
+
+//charAt()
+console.log("char At",all.charAt(10))
+
+//concat()
+console.log("concat method",all.concat(" i am stubent"))
+
+//endswith
+console.log("end with",all.endsWith("a"))
+console.log("end with",all.endsWith("n"))
+
+//includes
+console.log("include",all.includes("Ankit"))
+console.log("include" ,all.includes("bob"))
+
+//index ()
+console.log("indexof",all.indexOf("i"))
+
+//if not found
+console.log("indexof",all.indexOf("p"))
+
+//last index
+console.log("last indexof",all.lastIndexOf("i"))
+
+//specific index
+console.log("specific index find",all.indexOf("i",2))
+
+//length returns
+console.log("length",all.length)
+
