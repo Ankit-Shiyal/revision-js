a776494f6e89f383707b38123718df192e7edb82
ankit shiyal <ankitshiyal2005@gmail.com>
2026-06-28 15:12:35 +0530

    string added

---
+++ 05-string/2-string.js
@@ -0,0 +1,39 @@
+
+
+// Match
+let Name="Ankit";
+console.log("Match : ",Name.match("k"));
+
+console.log("Match :",Name.match("e"));
+
+//Repeat 
+console.log("Repeat :",Name.repeat(2))
+console.log("Repeat :",Name.repeat(4))
+
+
+// Replace
+console.log("Replace :",Name.replace("A","a"))
+console.log("Replace :",Name.replace("n","N"))
+
+
+// search
+console.log("search :",Name.search("p"))
+console.log("search :",Name.search("n"))
+
+// slice
+
+Name="Ankit Shiyal"
+
+console.log("Slice :",Name.slice(9))
+console.log("Slice :",Name.slice(5,10))
+console.log("Slice :",Name.slice(5,12))
+console.log("Slice :",Name.slice(-2))
+
+
+Name="Shiyal Ankit"
+
+console.log("Slice :",Name.slice(10))
+console.log("Slice :",Name.slice(3,7))
+console.log("Slice :",Name.slice(5,12))
+console.log("Slice :",Name.slice(-3))
+
