521e38d5477d8d6f2a91ec02aa0607a2ce0dbf0e
ankit shiyal <ankitshiyal2005@gmail.com>
2026-07-04 20:54:40 +0530

    string code added

---
+++ 05-string/3-string.js
@@ -0,0 +1,37 @@
+
+
+// split
+
+let Name="Ankit"
+
+console.log("split :",Name.split(" "))
+console.log("split :",Name.split("n"))
+console.log("split :",Name.split("A"))
+
+
+// start with
+console.log("start with :",Name.startsWith("n"))
+console.log("start with :",Name.startsWith("A"))
+
+// Lower case
+Name="Ankit"
+console.log("lower case :",Name.toLowerCase())
+
+fruit ="APPLE"
+console.log("lower case :",fruit.toLowerCase())
+
+
+// Upper case
+Name="ankit"
+console.log("upper case :",Name.toUpperCase())
+
+fruit ="banana"
+console.log("upper case :",fruit.toUpperCase())
+
+
+// trim
+Name="     ankit     "
+console.log("length start :",Name.length)
+console.log("trim start :",Name.trimStart().length)
+console.log("trim start :",Name.trimEnd().length)
+
