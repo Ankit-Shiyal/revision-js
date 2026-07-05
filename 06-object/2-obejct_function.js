5c2c4ee258100a4de18f2739f14624ca3258d081
ankit shiyal <ankitshiyal2005@gmail.com>
2026-07-05 16:05:54 +0530

    function with object added

---
+++ 06-object/2-obejct_function.js
@@ -0,0 +1,16 @@
+
+
+function Hotel(rating, room, staff){
+    this.rating = rating;
+    this.room = room;
+    this.staff = staff;
+}
+
+const hotel1 = new Hotel(5, 50, 50);
+console.log("Hotel detail 1:", hotel1);
+
+const hotel2 = new Hotel(4, 40, 30);
+console.log("Hotel detail 2:", hotel2);
+
+const hotel3 = new Hotel(3, 20, 15);
+console.log("Hotel detail 3:", hotel3);
