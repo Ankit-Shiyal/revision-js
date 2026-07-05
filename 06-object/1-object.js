6e02074afd23cf8ece71a184fd4cd9748af22095
ankit shiyal <ankitshiyal2005@gmail.com>
2026-07-05 16:04:54 +0530

    object code added

---
+++ 06-object/1-object.js
@@ -0,0 +1,23 @@
+
+
+// object literal syntex
+const person={
+    name:"Ankit",
+    lastName:"Shiyal",
+    city:"Bhavngar",
+    age:20
+}
+
+console.log(person)
+console.log(typeof person)
+
+// 
+
+const student = new Object();
+console.log(typeof student)
+
+student.name = "Ankit",
+student.exam="upsc",
+student.block="A"
+
+console.log("student")
