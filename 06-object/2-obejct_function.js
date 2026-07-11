6232578a4d51c8f495a2ed67d3b65075f0c0e741
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-07-11 11:03:50 +0530

    object code added
    Added a person object and demonstrated various ways to access its properties.

--- 06-object/2-obejct_function.js
+++ 06-object/2-obejct_function.js
@@ -14,3 +14,32 @@ console.log("Hotel detail 2:", hotel2);
 
 const hotel3 = new Hotel(3, 20, 15);
 console.log("Hotel detail 3:", hotel3);
+
+
+
+
+
+const person = {
+    name: "Ankit",
+    lastName: "Shiyal",
+    city: "Bhavnagar",
+    age: 20
+};
+
+console.log(person);
+
+// using . (dot)
+console.log(person.name);
+console.log(person.age);
+
+// using []
+console.log(person["name"]);
+console.log(person["age"]);
+
+// using destructuring
+const { name, lastName, city, age } = person;
+
+console.log(name);
+console.log(lastName);
+console.log(city);
+console.log(age);
