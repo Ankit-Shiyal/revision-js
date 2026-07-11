5aca067f2689171db29efc5f5aab597553df8bdf
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-07-11 11:05:13 +0530

    object code added

--- 06-object/2-obejct_function.js
+++ 06-object/2-obejct_function.js
@@ -43,3 +43,20 @@ console.log(name);
 console.log(lastName);
 console.log(city);
 console.log(age);
+
+
+
+const person = {
+    name: "Ankit",
+    lastName: "Shiyal",
+    city: "Bhavnagar",
+    age: 20
+};
+
+// using dot
+person.desgnation = "web Developer"
+console.log(person)
+
+//using []
+person["Blood-Group"] = "O+";
+console.log(person)
