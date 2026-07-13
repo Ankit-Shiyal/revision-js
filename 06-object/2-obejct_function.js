0d61e376085b0ea4c2452f4e4158da664cca7303
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-07-13 12:37:01 +0530

    Delete person object
    Removed person object and related console logs.

--- 06-object/2-obejct_function.js
+++ 06-object/2-obejct_function.js
@@ -17,31 +17,3 @@ console.log("Hotel detail 3:", hotel3);
 
 
 
-
-
-const person = {
-    name: "Ankit",
-    lastName: "Shiyal",
-    city: "Bhavnagar",
-    age: 20
-};
-
-console.log(person);
-
-// using . (dot)
-console.log(person.name);
-console.log(person.age);
-
-// using []
-console.log(person["name"]);
-console.log(person["age"]);
-
-// using destructuring
-const { name, lastName, city, age } = person;
-
-console.log(name);
-console.log(lastName);
-console.log(city);
-console.log(age);
-
-
