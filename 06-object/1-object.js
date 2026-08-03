92299402714b8eef2a08064e0940010a90134f6e
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-08-03 10:46:52 +0530

    Create student object with properties
    Added a student object with properties name, exam, and block.

--- 06-object/1-object.js
+++ 06-object/1-object.js
@@ -11,3 +11,13 @@ const person={
 console.log(person)
 console.log(typeof person)
 
+// 
+
+const student = new Object();
+console.log(typeof student)
+
+student.name = "Ankit",
+student.exam="upsc",
+student.block="A"
+
+console.log("student")
