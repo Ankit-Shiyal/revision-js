bbe6ab21aabbb11466477b4416329fb17f57472e
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-08-03 10:46:12 +0530

    Add hotel3 instance and log details

--- 06-object/2-obejct_function.js
+++ 06-object/2-obejct_function.js
@@ -12,6 +12,6 @@ console.log("Hotel detail 1:", hotel1);
 const hotel2 = new Hotel(4, 40, 30);
 console.log("Hotel detail 2:", hotel2);
 
-
-
+const hotel3 = new Hotel(3, 20, 15);
+console.log("Hotel detail 3:", hotel3);
 
